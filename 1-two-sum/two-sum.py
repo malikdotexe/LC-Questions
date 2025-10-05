ec2ddf35625096a0b7a825db83db1b2feb4606f7
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-05 13:55:26 +0530

    Update two-sum.py

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap= {}
+        seen= {}
         for i,num in enumerate(nums):
-            if (target-num) in hashmap:
+            if (target-num) in seen:
                 return [numset[target-num],i]
-            hashmap[num] = i
+            seen[num] = i
             
