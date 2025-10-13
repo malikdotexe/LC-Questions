3b02d35ffce9ee9bb4acbdf58e7af00177e8ba98
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-13 09:37:12 +0530

    Time: 3 ms (89.63%) | Memory: 17.7 MB (76.67%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:    
+        countn = [0]*26
+        for l in words[0]:
+            countn[ord(l)-ord('a')]+=1
+        stack = [countn]
+        res=[words[0]]
+        for word in words:
+            countn = [0]*26
+            for l in word:
+                countn[ord(l)-ord('a')]+=1
+            if stack[-1] != countn:
+                stack.append(countn)
+                res.append(word)
+        return res
