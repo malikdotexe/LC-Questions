2bb8cebe05f81a48bf665c88b35f606997b113d9
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-13 11:18:07 +0530

    Improve comment clarity in lengthOfLongestSubstring
    Clarified comment on sliding window approach for substring calculation.

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
    
-        #sliding window approach, here we initialize a Count hashmap to store the frequency of letters, if after including a letter , its frequency goes above 1 we need to keep shrinking our string from left (while reducing the frequency of the letter which is getting excluded) till it comes back to 1. And at each step we update the maxlen(r-l+1)
+        #sliding window approach, here we first initialize a count hashmap to store the frequency of letters, if after including a letter , its frequency goes above 1 we need to keep shrinking our string from left (while reducing the frequency of the letter which is getting excluded) till it comes back to 1. And at each step we update the maxlen(r-l+1)
         l = 0
         maxlen = 0
         Count = defaultdict(int)
