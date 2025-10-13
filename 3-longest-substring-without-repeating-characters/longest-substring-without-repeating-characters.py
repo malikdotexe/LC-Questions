b90aeb9441b5dc4e1c5962dcae9f181dd18d7b87
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-13 10:37:18 +0530

    Time: 23 ms (29.37%) | Memory: 17.9 MB (52.73%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,7 +6,7 @@ class Solution:
         
         #sliding window approach, here we are initializing a Count hashmap to store the frequency of letters, if after including a letter , its frequency goes above 1 we need to keep shrinking our string from left (while reducing the frequency of the letter which is getting excluded) till it comes back to 1. And at each step we update the maxlen(r-l+1)
         l = 0
-        maxlen = float("-inf")
+        maxlen = 0
         Count = defaultdict(int)
         for r in range(len(s)):
             Count[s[r]] +=1
