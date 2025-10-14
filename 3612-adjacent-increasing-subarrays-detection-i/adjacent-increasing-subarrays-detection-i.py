7eee5404140fa5c2f9a4a3759496d11b25f13087
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-14 12:27:01 +0530

    Time: 93 ms (21.39%) | Memory: 18 MB (25.67%) - LeetSync

---
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
@@ -0,0 +1,23 @@
+from collections import deque
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        #lenarr would contain length of all increasing subarrays
+        lenarr = [1]
+        l =1
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                l+=1
+            if nums[i]<=nums[i-1] or i==len(nums)-1:
+                print(nums[i])
+                lenarr.append(l)
+                print(lenarr)
+                #if we can make two increasing subarrays from the same subarray each of length k
+                if lenarr[-1]//2>=k:
+                    return True
+                #elif there are two unique increasing subarrays adjacent
+                elif len(lenarr)>1 and lenarr[-1]>=k and lenarr[-2]>=k:
+                    return True
+                l = 1
+               
+                
+        return False
