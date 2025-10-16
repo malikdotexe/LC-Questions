9e29eda64567db62147e0ac23fbf21dcae3478a6
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-16 09:54:40 +0530

    Time: 189 ms (29.25%) | Memory: 38.5 MB (58.49%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
@@ -1,26 +1,16 @@
-from collections import defaultdict
+from collections import Counter
+
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
         #Frequency map for Remainder
-        remfreq =defaultdict(int)
-        #Set for storing all the values we could attain by doing ops
-        ans = set()
-
-        for num in nums:
-            rem = num%value
-            #If we are seeing the remainder for the first time we would simply add that to our set
-            if remfreq[rem]==0:
-                remfreq[rem]+=1
-                ans.add(rem)
-            #else there is no benefit in adding the same remainder rather we would use it go to a higher number by adding our value to it. Eg if value = 3 and we have "0" as a remainder already in set we would add 0+3 to the set and later if we found 0 remainder again we would add 0+3+3 to the set
-            else:
-                count = remfreq[rem]
-                ans.add(rem+(value*(count)))
-                remfreq[num%value]+=1
-        #Simple loop to find where the sequence breaks
-        maximum = max(ans)
-        for i in range(0,maximum):
-            if i not in ans:
+        remfreq = Counter(num % value for num in nums)
+        i = 0
+        while True:
+            r = i % value
+            #If we run out of that remainder
+            if remfreq[r] == 0:
                 return i
-        #Edge case where we have complete sequence
-        return maximum+1
+            #Decrease count of the remainder we have used
+            remfreq[r] -= 1
+            #i starts from 0 and goes till its remainder with value is not found
+            i += 1
