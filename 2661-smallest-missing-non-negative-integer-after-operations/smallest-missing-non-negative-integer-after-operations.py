cb683800f57d80eb0ffb1f9ec405c4d461cd8196
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-16 09:49:10 +0530

    Time: 210 ms (12.82%) | Memory: 44.5 MB (5.40%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
@@ -5,8 +5,6 @@ class Solution:
         remfreq =defaultdict(int)
         #Set for storing all the values we could attain by doing ops
         ans = set()
-        #Storing maximum(to be used in our last loop)
-        maximum =-1
 
         for num in nums:
             rem = num%value
@@ -14,15 +12,14 @@ class Solution:
             if remfreq[rem]==0:
                 remfreq[rem]+=1
                 ans.add(rem)
-                maximum = max(rem,maximum)
             #else there is no benefit in adding the same remainder rather we would use it go to a higher number by adding our value to it. Eg if value = 3 and we have "0" as a remainder already in set we would add 0+3 to the set and later if we found 0 remainder again we would add 0+3+3 to the set
             else:
                 count = remfreq[rem]
                 ans.add(rem+(value*(count)))
-                maximum = max(rem+(value*(count)),maximum)
                 remfreq[num%value]+=1
         #Simple loop to find where the sequence breaks
-        for i in range(0,maximum+1):
+        maximum = max(ans)
+        for i in range(0,maximum):
             if i not in ans:
                 return i
         #Edge case where we have complete sequence
