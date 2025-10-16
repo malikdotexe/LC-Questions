54c18197e1a7cf0e4f00429c94341a1244964d26
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-16 09:47:20 +0530

    Time: 298 ms (5.06%) | Memory: 44.5 MB (5.40%) - LeetSync

---
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+class Solution:
+    def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        #Frequency map for Remainder
+        remfreq =defaultdict(int)
+        #Set for storing all the values we could attain by doing ops
+        ans = set()
+        #Storing maximum(to be used in our last loop)
+        maximum =-1
+
+        for num in nums:
+            rem = num%value
+            #If we are seeing the remainder for the first time we would simply add that to our set
+            if remfreq[rem]==0:
+                remfreq[rem]+=1
+                ans.add(rem)
+                maximum = max(rem,maximum)
+            #else there is no benefit in adding the same remainder rather we would use it go to a higher number by adding our value to it. Eg if value = 3 and we have "0" as a remainder already in set we would add 0+3 to the set and later if we found 0 remainder again we would add 0+3+3 to the set
+            else:
+                count = remfreq[rem]
+                ans.add(rem+(value*(count)))
+                maximum = max(rem+(value*(count)),maximum)
+                remfreq[num%value]+=1
+        #Simple loop to find where the sequence breaks
+        for i in range(0,maximum+1):
+            if i not in ans:
+                return i
+        #Edge case where we have complete sequence
+        return maximum+1
