874793dbbfb928c316bf1bf7167ccea25dee61ed
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-22 10:26:03 +0530

    Time: 112 ms (79.14%) | Memory: 18 MB (49.26%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,11 +5,12 @@ class Solution:
         l= 0
         Count = defaultdict(int)
         maxlen = 0
-
+        maxfreq = 0
         for r in range(len(s)):
             Count[s[r]] +=1
             #We will shrink from left till we get a valid string
-            while (r-l+1)-max(Count.values())>k :
+            maxfreq = max(maxfreq,Count[s[r]])
+            while ((r-l+1)-maxfreq)>k :
                 Count[s[l]]-=1
                 l+=1
             #update maxlen if current valid string length is bigger
