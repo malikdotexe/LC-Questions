cec6de695385f4f230b2a1a6d7dbe2c788a41a61
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-27 12:54:56 +0530

    Time: 79 ms (27.68%) | Memory: 19.5 MB (77.78%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        i=1
+        res = 0
+        fdevices = len([x for x in bank[0] if x=="1"])
+        while i<len(bank):
+            sdevices = len([x for x in bank[i] if x=="1"])
+            print(f"fdevices - {fdevices} and sdevices {sdevices} and res {res}")
+            if sdevices>0:
+                res += fdevices * sdevices
+                fdevices = sdevices
+            i+=1
+        return res
+
+
