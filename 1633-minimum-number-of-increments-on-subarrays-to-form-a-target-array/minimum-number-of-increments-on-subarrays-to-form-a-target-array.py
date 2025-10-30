1cd951223ecc9fb9d6a18d889c20c8188a80b5d1
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-10-30 10:32:48 +0530

    Time: 27 ms (77.65%) | Memory: 26.6 MB (96.16%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -1,38 +1,13 @@
-#Similar to largest rectangle in histogr
+#Simpler Approach
 class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
         n = len(target)
-        #ns = next smaller
-        ns = [-1]*n
-        stack = []
-        for i in range(n-1,-1,-1):
-            while stack and target[i]<=stack[-1]:
-                stack.pop()
-            if not stack:
-                ns[i] = 0
-            else:
-                ns[i] = stack[-1]
-            stack.append(target[i])
-        #ps = previous smaller
-        ps = [-1]*n
-        stack = []
-        for i in range(n):
-            while stack and target[i]<stack[-1]:
-                stack.pop()
-            if not stack:
-                ps[i]=0
-            else:
-                ps[i]= stack[-1]
-            stack.append(target[i])
-        ans = 0
-        #traversing target
-        for i in range(n):
-            if i>0 and target[i] == target[i-1]:
-                continue
-            else:
-                ans += target[i] - max(ns[i],ps[i])
+        ans = target[0]
+        for i in range(1,n):
+            if target[i]>target[i-1]:
+                ans+=target[i]-target[i-1]
         return ans
-                    
+        
 
 
                 
