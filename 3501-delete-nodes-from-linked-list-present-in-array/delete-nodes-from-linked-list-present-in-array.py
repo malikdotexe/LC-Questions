e7554ffa1982dcec018af95dafa5ad83ea67beea
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-11-01 12:47:31 +0530

    Time: 51 ms (88.23%) | Memory: 58.7 MB (22.26%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -1,22 +1,19 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        dummy = prev = ListNode(0,head)
+        tail = head
         nums = set(nums)
-        #deleting starting nodes if they are in nums by shifting head 
-        while head and head.val in nums:
-            head = head.next
-        
-        curr = head
-        #deleting nodes in place
-        while curr and curr.next: #as we are accessing curr.next.val
-            if curr.next.val in nums:
-                #Deleting the node by skipping and indirectly traversing too as now curr.next.val in the if condition would retrieve the new nodes value
-                curr.next= curr.next.next
-            else:
-                curr= curr.next
-        return head
+        while True:
+            #selecting range
+            while tail and tail.val in nums:
+                tail = tail.next
+            #deleting node (tail is on the node next to the node to be deleted)
+            prev.next= tail
+            #updating pointer
+            if not tail:
+                break
+            
+            tail = tail.next
+            prev = prev.next
+            
+        return dummy.next
