0a010aae8d97d2c59e48b79e58cc9c573c5cee7c
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-11-04 11:24:06 +0530

    Time: 31 ms (25.02%) | Memory: 18.5 MB (59.14%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+        for i in range(n+1):
+            res.append(self.countones(i))
+        return res
+
+    def countones(self,n):
+        res = 0 
+        while n:
+            n = n&(n-1)
+            res+=1
+        return res
