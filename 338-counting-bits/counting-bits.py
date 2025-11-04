29f168805cfaf2b9a3e8691e2bb326cae4670c3d
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-11-04 12:01:29 +0530

    Time: 4 ms (67.78%) | Memory: 18.6 MB (59.14%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,21 +1,16 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # First solution formula 1+dp[n-offset]
-        # We use the precomputed result for the difference between n and the largest power of 2 less than or equal to n, then add 1 for the new set bit introduced by that power of 2.
-        # n-binary-numberofones
-        # 0-00-0   
-        # 1-01-1    1+dp[1-1] 
-        # 2-10-1    1+dp[2-2] new offset 
-        # 3-11-2    1+dp[3-2]
-        # 4-100-1   1+dp[4-4] new offset
-        # 5-101-2   1+dp[5-4] computing new range(4-8) using previous range(0-4) and just addin 1  
-        # 6-110-2   1+dp[6-4]
-        # 7-111-3   1+dp[7-4]
-        # 8-1000-1  1+dp[8-8] computing new range(8-16) using previous range(0-8) and just addin 1
+        #Second approach
+        #for even numbers ,number of set bits(1) are same as that of the number//2
+        #Eg - 14 has same set bits as 7 
+        #for odd nums, its same as the set bits of (number//2) + 1
+        #Eg - 7 has same set bits as (7/2 = 3) + 1 => 3 has 2 set bits ,adding 1 gives us 3
         dp = [0]*(n+1)
-        offset = 1
         for i in range(1,n+1):
-            if offset*2==i:
-                offset = i
-            dp[i]= 1+dp[i-offset]
+            #even
+            if i%2==0:
+                dp[i]=dp[i//2]
+            #odd 
+            elif i%2!=0:
+                dp[i]=dp[i//2]+1
         return dp
