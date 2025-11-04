4497d418ac268ec9e33e6e1a44cc54a51e700d8f
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-11-04 08:40:43 +0530

    Time: 3 ms (90.50%) | Memory: 21.3 MB (61.77%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,10 @@
+#with minheap
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = Counter(nums)
-        bucketarr = [[] for i in range(len(nums)+1)]
-        for num,freq in hashmap.items():
-            bucketarr[freq].append(num)
-        res = []
-
-        for i in range(len(bucketarr)-1,-1,-1):
-            for num in bucketarr[i]:
-                res.append(num)
-                if len(res)==k:
-                    return res
+        count = Counter(nums)
+        countarr = [[y,x] for x,y in count.items()]
+        heapq.heapify(countarr)
+        while len(countarr)>k:
+            heapq.heappop(countarr)
+        res = [x[1] for x in countarr]
+        return res
