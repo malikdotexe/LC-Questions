6e48e52ef840d4f11e30b36781e8b8936befdf5e
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-11-04 12:18:59 +0530

    Time: 4 ms (67.78%) | Memory: 18.4 MB (90.02%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,16 +1,10 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        #Second approach
-        #for even numbers ,number of set bits(1) are same as that of the number//2
-        #Eg - 14 has same set bits as 7 
-        #for odd nums, its same as the set bits of (number//2) + 1
-        #Eg - 7 has same set bits as (7/2 = 3) + 1 => 3 has 2 set bits ,adding 1 gives us 3
-        dp = [0]*(n+1)
+        #Third Approach
+        #n>>1 removes the lsb
+        #n&1 returns 1 if last bit is 1 else 0
+        #So in this dp approach we are using precomputed value of the number with one bit less than adding 1 or 0 depending if our number has 1 or 0 at the end
+        dp = [0]* (n+1)
         for i in range(1,n+1):
-            #even
-            if i%2==0:
-                dp[i]=dp[i//2]
-            #odd 
-            elif i%2!=0:
-                dp[i]=dp[i//2]+1
+            dp[i] = dp[i>>1]+ (i&1)
         return dp
