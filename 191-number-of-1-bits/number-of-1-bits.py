3da07a6b7d54c0f92fe00e170b6d2ecb8ba20f6b
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-11-04 10:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.26%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,9 +1,21 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        #Second Solution
+        #If we AND n with n-1 we can remove 1 , and the number of times we can do that would be our answer
+        res = 0
+        while n!=0:
+            n = n&(n-1)
+            res+=1
+        return res
+        
+        
+        
         #First solution
         #modding by 2 returns 1 if last bit is 1 else 0 , and then we right shift by 1 (>>1) to do the operation on new bit until there are no bits left
-        res = 0 
-        while n!=0:
-            res += n%2
-            n = n>>1
-        return res
+        # res = 0 
+        # while n!=0:
+        #     res += n%2
+        #     n = n>>1
+        # return res
+        
+
