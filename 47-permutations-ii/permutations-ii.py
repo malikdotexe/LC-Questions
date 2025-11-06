981db377eabe8027f95b67b01554f9ba43b4f8cb
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-11-06 15:31:23 +0530

    Time: 636 ms (5.00%) | Memory: 18.1 MB (35.25%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,24 +1,25 @@
 class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res = []
+    def permuteUnique(self, nums):
+        result = []
         temp = []
-        used = [0]*len(nums)
-        
+        used = [False] * len(nums)
+
         def backtrack():
-            if len(temp)==len(nums) and temp not in res:
-                res.append(temp.copy())
+            if len(temp) == len(nums) and temp not in result:
+                result.append(temp.copy())
                 return
 
-
-            for i,num in enumerate(nums):
+            for i in range(len(nums)):
                 if used[i]:
                     continue
-                used[i]=1
-                temp.append(num)
+
+                used[i] = True
+                temp.append(nums[i])
+
                 backtrack()
-                used[i]=0
+
+                used[i] = False
                 temp.pop()
-                
 
         backtrack()
-        return res
+        return result
