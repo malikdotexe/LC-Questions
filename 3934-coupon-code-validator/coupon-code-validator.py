8d9b4d82eb75e29bf6b221cdb1d72c688bb2708c
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-12-16 19:05:28 +0530

    Time: 14 ms (23.23%) | Memory: 18.2 MB (13.00%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.py
+++ 3934-coupon-code-validator/coupon-code-validator.py
@@ -9,10 +9,10 @@ class Solution:
         for i in range(len(code)):
             if re.fullmatch(r"[A-Za-z0-9_]+", code[i]) and businessLine[i] in validcode and isActive[i]:
                 validcode[businessLine[i]].append(code[i])
-                validcode[businessLine[i]].sort()
         res = []
-      
+   
         for codes in validcode.values():
+            codes.sort()
             for code in codes:
                 res.append(code)
         return res
