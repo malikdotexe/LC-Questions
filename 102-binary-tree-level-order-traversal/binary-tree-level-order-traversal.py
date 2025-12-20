108badbb4061cb486814a746e42bf194a2ff21b1
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-12-20 22:32:30 +0530

    Time: 3 ms (20.13%) | Memory: 17.9 MB (99.91%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -3,17 +3,15 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        queue = deque([root])
-        res = []
-        while queue:
-            level = []
-            for i in range(len(queue)):
-                curr = queue.popleft()
-                level.append(curr.val)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-            res.append(level)
-        
-        return res
+        q = deque()
+        q.append(root)
+        level = []
+        while q:
+            level.append([node.val for node in q])
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return level
