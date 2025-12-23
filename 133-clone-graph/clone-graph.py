e200c03669a5c6f14d8baaec773a66a5df8b1c45
Piyush Malik <127580405+malikdotexe@users.noreply.github.com>
2025-12-23 21:05:27 +0530

    Time: 37 ms (89.14%) | Memory: 17.7 MB (98.32%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,14 +9,25 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        #We could not directly use a visited set because 
         oldToNew = {}
         def clone(node):
+            # If node is already cloned, return its copy
             if node in oldToNew:
                 return oldToNew[node]
+            #creating the copy node 
             copy = Node(node.val)
+            #Assigning copy node to original node
             oldToNew[node] = copy
             for neig in node.neighbors:
                 oldToNew[node].neighbors.append(clone(neig))
+
+            #return cloned node
             return copy
         return clone(node) if node else None
-        
+
+#A boolean visited array only tells whether a node was seen, but it does not store the address of the cloned node.
+
+# While cloning a graph, the same node can be reached again through a different path, and we must reuse the already created clone instead of creating a new one.
+
+# To correctly connect edges, we need direct access to the cloned node, which is not possible with just a boolean.
